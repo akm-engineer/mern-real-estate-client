014c51105839c449319f7d7854d6ac8fb42d4521
Ashish Mishra <ashishgk1999@gmail.com>
2023-12-31 16:54:21 +0530

    Db connection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log(`DB is connected`);
+  })
+  .catch((e) => console.log(e));
 const app = express();
 
 app.listen(4000, () => {
   console.log(`Server is running on 4000`);
 });
+//ashishgk1999
+//TOle2PnNjmwlur8i
+//mongodb+srv://ashishgk1999:<password>@mer-estate.agwxjau.mongodb.net/?retryWrites=true&w=majority
