2c9511c881098cd2914b4fb55bcf8fc38a40893e
Ashish Mishra <ashishgk1999@gmail.com>
2023-12-31 17:10:05 +0530

    create user model

---
+++ backend/models/user.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: "Please enter your name",
+      unique: true,
+      trim: true,
+      maxlength: 32,
+    },
+    email: {
+      type: String,
+      require: "Please enter a email",
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      require: "Please enter a password",
+      minLength: 8,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
