b5fb351fea2921a8b74c8912cd6272e29db69a90
Ashish Mishra <ashishgk1999@gmail.com>
2024-01-01 17:34:16 +0530

    create a middleware function to handle possible error

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,12 @@ app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/auth", authRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({ success: false, statusCode, message });
+});
+
 app.listen(4000, () => {
   console.log(`Server is running on 4000`);
 });
